96b1ad38b66de382f7417c1b23415fea3d23d608
Alex Le Mesurier <alexlem17@gmail.com>
2017-10-29 17:58:29 +0400

    todays changes

---
+++ Extech/Assets/scripts/objectives.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objectives : MonoBehaviour {
+
+    public string objectTiveText;
+    public Text objText;
+
+    public bool isActive = false;
+
+    public float objectiveTime = 0f;
+
+    private void Update()
+    {
+        objText.text = objectTiveText;
+
+        if(isActive == true && objectiveTime < 0)
+        {
+            objectiveTime -= Time.deltaTime;
+            objText.text = objectiveTime.ToString() + objectTiveText;
+		}
+    }
+
+    public void modText(string text)
+    {
+        objectTiveText = text;
+    }
+
+}
